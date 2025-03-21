81dbc0d96234567552ca70526131f88bb6fded27
nono <nour98nour@gmail.com>
2025-03-21 17:51:43 +0200

    Update README and clean up middleware comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -50,7 +50,6 @@ export async function updateSession(request: NextRequest) {
   // Do not run code between createServerClient and
   // supabase.auth.getUser(). A simple mistake could make it very hard to debug
   // issues with users being randomly logged out.
-
   // IMPORTANT: DO NOT REMOVE auth.getUser()
 
   if (isAuthRoute) {

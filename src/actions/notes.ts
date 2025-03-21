44c37f53c48898b7de5ba38d170158bcff92aca1
nono <nour98nour@gmail.com>
2025-03-21 03:56:20 +0200

    Add UUID generation and create note action; update NewNoteButton component

--- src/actions/notes.ts
+++ src/actions/notes.ts
@@ -4,10 +4,27 @@ import { getUser } from "@/app/auth/server";
 import { prisma } from "@/db/prisma";
 import { handleError } from "@/lib/utils";
 
+export const createNoteAction = async (noteId: string) => {
+  try {
+    const user = await getUser();
+    if (!user) throw new Error("You must be logged in to create a note");
+    await prisma.note.create({
+      data: {
+        id: noteId,
+        authorId: user.id,
+        text: "",
+      },
+    });
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
 export const updateNoteAction = async (noteId: string, text: string) => {
   try {
     const user = await getUser();
-    if (!user) throw new Error("User not found");
+    if (!user) throw new Error("You must be logged in to update a note");
     await prisma.note.update({
       where: {id: noteId},
       data: {text},
--- src/components/NewNoteButton.tsx
+++ src/components/NewNoteButton.tsx
@@ -1,16 +1,42 @@
 "use client";
 
 import { User } from "@supabase/supabase-js";
+import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
+import { createNoteAction } from "@/actions/notes";
 
 type Props = {
     user: User | null;
 }
 
 function NewNoteButton( { user }: Props ) {
-  console.log(user);
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const handleClickNewNoteButton = async () => {
+    if (!user) {
+      router.push("/login");
+    } else {
+      setLoading(true);
+      const uuid = uuidv4();
+      await createNoteAction(uuid);
+      router.push(`/?noteId=${uuid}&toastType=newNote`);
+      setLoading(false);
+    }
+  };
   
     return (
-    <div>NewNoteButton</div>
+    <Button
+      onClick={handleClickNewNoteButton}
+      variant={"secondary"}
+      className="fixed bottom-4 right-4"
+      disabled={loading}
+    >
+      {loading ? <Loader2 className="animate-spin"/> : "New Note"}
+    </Button>
   )
 }
 
